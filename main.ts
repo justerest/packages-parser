74dcb4b4b45281bd04d494729836d04ac8690a64
andrey <aamatveef@gmail.com>
2018-06-07 11:26:06 +0500

    feat: add async get for repositories

--- main.ts
+++ main.ts
@@ -1,29 +1,80 @@
 import chalk from 'chalk';
-import { copy } from 'copy-paste';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import fetch from 'node-fetch';
 
 const ARGS = process.argv.slice(2);
 
-const depStr = ARGS.length
-	? ARGS.map(parseDepStr).join('\n')
-	: parseDepStr('./package.json');
+(async function main() {
+  const files = ARGS.length
+    ? ARGS.map(parseDependencies)
+    : [parseDependencies('./package.json')];
 
-const rows = depStr.replace(/:/g, ';').replace(/,/g, '\n');
+  const allDeps = await Promise.all(files);
 
-copy(rows);
-console.log(chalk.blue(`\n${rows}\n`));
-console.log('it\'s already copied to clipboard');
+  const list = allDeps.join('\n')
+    .replace(/\n\n/g, '\n')
+    .replace(/:(.\d*\.\d*\.\d*|\w*)/g, '')
+    .replace(/,/g, '\n')
+    .split('\n');
 
-function parseDepStr(path: string): string {
-	const dirtyList = readFileSync(path, 'utf-8')
-		.replace(/(\n|\s|")/g, '')
-		.match(/dependencies:{[a-z0-9:^.@\/\-,]*}/gi);
+  const uniqueList = Array.from(new Set(list));
 
-	if (dirtyList) {
-		return dirtyList.map((dirtyDepStr) => dirtyDepStr.slice(14, -1)).join(',');
-	}
-	else {
-		console.warn(chalk.yellow(`Dependencies fields not found in ${path}`));
-		return '';
-	}
+  const result = uniqueList
+    .sort()
+    .map((s) => s + ';Последняя версия')
+    .map((s) => s + ';Сообщество разработчиков')
+    .map((s) => s + ';\\\\orgName')
+    .map((s) => s + ';децентрализованная')
+    .map((s) => s + ';\\\\orgName')
+    .map((s) => s + ';\\\\orgName')
+    .map((s) => s + ';нет')
+    .map((s) => s + ';нет')
+    .map((s) => s + ';сегмент разработки ЛВС \\\\orgName')
+    .map(addSite)
+    .map((s) => s + ';Производителем не установлен')
+    .map((s) => s + ';Средство для web-разработки')
+    .map((s) => s + ';\\\\orgName')
+    .map((s) => s + ';\\\\docNumber')
+    .join('\n');
+
+  writeFileSync('./parsed-packages.csv', result);
+  console.log(chalk.greenBright(`Completed. Load ${uniqueList.length}`));
+})();
+
+async function parseDependencies(path: string): Promise<string> {
+  try {
+    const text = path.match(/^http/i)
+      ? await getPkgJsonFromGithub(path)
+      : readFileSync(path, 'utf-8');
+
+    const dirtyList = text
+      .replace(/(\n|\s|")/g, '')
+      .match(/dependencies:{[a-z0-9:^.@\/\-,]*}/gi);
+
+    if (dirtyList) {
+      return dirtyList.map((dirtyDepStr) => dirtyDepStr.slice(14, -1)).join(',');
+    }
+    else {
+      warnNotFound(path);
+      return '';
+    }
+  }
+  catch (e) {
+    warnNotFound(path);
+    return '';
+  }
+}
+
+function getPkgJsonFromGithub(projectPath: string): Promise<string> {
+  const link = projectPath.replace('github', 'raw.githubusercontent') + '/master/package.json';
+  return fetch(link).then((res) => res.text());
+}
+
+function addSite(pkgInfo: string) {
+  const [name] = pkgInfo.split(';');
+  return pkgInfo + ';https://www.npmjs.com/package/' + name;
+}
+
+function warnNotFound(path: string) {
+  console.warn(chalk.yellow(`Dependencies fields not found in ${path}`));
 }
